9839c2027663bc122b1a54e473ec9d4678ee9941
Romain Vimont <rom@rom1v.com>
2023-03-02 18:07:38 +0100

    Fix new protocol
    First write the endpoint, then read the sync byte.
    
    https://gitlab.com/kyber-labs/kyber-streamer/kymux/#local-kymux-client-connection

--- src/main.rs
+++ src/main.rs
@@ -93,8 +93,9 @@ fn main() -> Result<(), StreamerError> {
 
     let start = Instant::now();
 
-    tcp_stream.read(&mut [0u8; 8])?; // consume endpoint (don't care)
-    tcp_stream.write(&[0u8])?; // sync byte
+    tcp_stream.write(&kymux_addr.endpoint_id.to_be_bytes())?;
+
+    tcp_stream.read(&mut [0u8])?; // sync byte
 
     let sid_and_codec_packet = [0, 0, 0, 0, b'h', b'2', b'6', b'4', 0, 0, 0, 0, 0, 0, 0, 0];
     tcp_stream.write(&sid_and_codec_packet)?;
